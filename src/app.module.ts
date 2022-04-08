0e6e8e36c060e7efeca818d17cc6dd754ae3c79e
unknown <Student@108-24.vssdomena.scv.si>
2022-04-08 11:46:35 +0200

    CRUD funkcija

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+      UserModule,
+    TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'node22',
+      autoLoadEntities: true,
+    entities: [],
+    synchronize: true,
+  }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
